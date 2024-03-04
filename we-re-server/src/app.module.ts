21174aa78f59c8e4f55312c53b1fc89bef1ab6fa
rjsdn0124 <201802145@o.cnu.ac.kr>
2024-03-04 20:35:17 +0900

    [NEW] Added mysql to project

--- we-re-server/src/app.module.ts
+++ we-re-server/src/app.module.ts
@@ -2,11 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [],
+      synchronize: process.env.NODE_ENV !== 'production',
     }),
   ],
   controllers: [AppController],
