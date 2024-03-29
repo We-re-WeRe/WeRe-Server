1d132b85ff8457cbcc44477e340b9ba7c77e694f
Geonu Lim <44187187+rjsdn0124@users.noreply.github.com>
2024-03-29 17:52:42 +0900

    Feature/swagger (#30)
    * [PACKAGE] added swagger in package.json
    
    * [FIX] add versioning for swagger docs.
    
    * [REMOVE] remove unnecessary annotation
    
    * [FEATURE] point api description and fixed some typo

--- we-re-server/src/main.ts
+++ we-re-server/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { version } from '../package.json';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('We-Re API Document')
+    .setDescription('WeRe API description')
+    .setVersion(version)
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
