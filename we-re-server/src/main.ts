78881bcfb1a96cfd87f16c9121465d25f1aab0d4
rjsdn0124 <201802145@o.cnu.ac.kr>
2024-03-29 17:19:57 +0900

    [FIX] add versioning for swagger docs.

--- we-re-server/src/main.ts
+++ we-re-server/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { version } from '../package.json';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const config = new DocumentBuilder()
+    .setTitle('We-Re API Document')
+    .setDescription('WeRe API description')
+    .setVersion(version)
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
