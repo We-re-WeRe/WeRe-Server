3973757b3d4be8e8603662a9c17dff0754020f84
rjsdn0124 <201802145@o.cnu.ac.kr>
2024-03-30 23:54:25 +0900

    [NEW] added pipe to validate data type

--- we-re-server/src/main.ts
+++ we-re-server/src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { version } from '../package.json';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Swagger 설정.
   const config = new DocumentBuilder()
     .setTitle('We-Re API Document')
     .setDescription('WeRe API description')
@@ -12,6 +14,15 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  // 파이프 선언.
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
   await app.listen(3000);
 }
