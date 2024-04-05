8ffbd43570106e852e7b88f765dec7440285fc84
rjsdn0124 <201802145@o.cnu.ac.kr>
2024-04-05 18:19:47 +0900

    [FEATURE] implemented create point dto.

--- we-re-server/src/points/dto/create-point.dto.ts
+++ we-re-server/src/points/dto/create-point.dto.ts
@@ -1,1 +1,25 @@
-export class CreatePointDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Exclude, Expose } from 'class-transformer';
+import { IsEnum, IsInt, IsNotEmpty, IsOptional } from 'class-validator';
+import { REASON, Reason } from 'src/entities/point.entity';
+
+@Exclude()
+export class CreatePointDto {
+  @ApiProperty()
+  @Expose()
+  @IsInt()
+  @IsNotEmpty()
+  userId: number;
+
+  @ApiProperty()
+  @Expose()
+  @IsNotEmpty()
+  @IsEnum(REASON)
+  reason: Reason;
+
+  @ApiProperty()
+  @Expose()
+  @IsInt()
+  @IsOptional()
+  mount?: number;
+}
