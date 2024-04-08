d0126621ed3bc724b230a1cb43d258b89a370879
rjsdn0124 <201802145@o.cnu.ac.kr>
2024-04-08 18:54:18 +0900

    [MIGRATION] move follow dto to it's own file from create-user-dto.

--- we-re-server/src/users/dto/create-user.dto.ts
+++ we-re-server/src/users/dto/create-user.dto.ts
@@ -2,14 +2,3 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsInt, IsNotEmpty } from 'class-validator';
 
 export class CreateUserDto {}
-export class CreateFollowDto {
-  @ApiProperty()
-  @IsInt()
-  @IsNotEmpty()
-  id: number;
-
-  @ApiProperty()
-  @IsInt()
-  @IsNotEmpty()
-  targetId: number;
-}
