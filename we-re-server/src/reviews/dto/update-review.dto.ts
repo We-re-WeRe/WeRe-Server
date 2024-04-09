400808d8a53a26a36cc67cee3260fc064539bf8d
rjsdn0124 <201802145@o.cnu.ac.kr>
2024-04-09 16:37:06 +0900

    [REFACTOR] change to do not use create dto at update dto to see schema in swagger.

--- we-re-server/src/reviews/dto/update-review.dto.ts
+++ we-re-server/src/reviews/dto/update-review.dto.ts
@@ -1,11 +1,19 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateReviewDto } from './create-review.dto';
 import { ApiProperty } from '@nestjs/swagger';
-import { IsInt, IsNotEmpty } from 'class-validator';
+import { IsInt, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
-export class UpdateReviewDto extends PartialType(CreateReviewDto) {
+export class UpdateReviewDto {
   @ApiProperty()
   @IsInt()
   @IsNotEmpty()
   id: number;
+
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  contents?: string;
+
+  @ApiProperty()
+  @IsInt()
+  @IsOptional()
+  starPoint?: number;
 }
