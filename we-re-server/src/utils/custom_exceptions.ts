3b5383258bc47607d355bb4d0469a6e27198c2dd
rjsdn0124 <201802145@o.cnu.ac.kr>
2024-04-18 20:28:24 +0900

    [FEATURE] add cause in custom exceptions.

--- we-re-server/src/utils/custom_exceptions.ts
+++ we-re-server/src/utils/custom_exceptions.ts
@@ -6,33 +6,36 @@ import {
 } from '@nestjs/common';
 
 export class CustomBadRequestException extends BadRequestException {
-  constructor(message: string) {
+  constructor(cause: string) {
     // TODO::에러 메시지를 좀 더 특정해서 알려주자. 인자로 뭐가 문제인지 받아옵시다.
     super({
       statusCode: HttpStatus.BAD_REQUEST,
-      message: 'Please check your Params again. ' + message,
+      message: 'Please check your Params again.',
       error: 'Bad Request',
+      cause,
     });
   }
 }
 
 export class CustomUnauthorziedException extends UnauthorizedException {
-  constructor(message: string) {
+  constructor(cause: string) {
     // TODO::에러 메시지를 좀 더 특정해서 알려주자. 인자로 뭐가 문제인지 받아옵시다.
     super({
       statusCode: HttpStatus.UNAUTHORIZED,
-      message: 'Please check your Token again. ' + message,
+      message: 'Please check your Token again.',
       error: 'Unauthorized',
+      cause,
     });
   }
 }
 
 export class CustomNotFoundException extends NotFoundException {
-  constructor(message: string) {
+  constructor(cause: string) {
     super({
       statusCode: HttpStatus.NOT_FOUND,
-      message: 'Please check your Path again.' + message,
+      message: 'Please check your Path again.',
       error: 'Not Found',
+      cause,
     });
   }
 }
