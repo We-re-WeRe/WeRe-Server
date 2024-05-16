f3d4a481fef72cdce22c6fef0201acf9a3d6d5d8
rjsdn0124 <201802145@o.cnu.ac.kr>
2024-05-16 17:17:32 +0900

    [FEATURE] add guard in provider as global.

--- we-re-server/src/auth/auth.module.ts
+++ we-re-server/src/auth/auth.module.ts
@@ -5,6 +5,8 @@ import { AuthRepository } from './auth.repository';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from 'src/users/users.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AccessTokenGuard } from './auth.guard';
 
 @Module({
   imports: [
@@ -22,7 +24,14 @@ import { UsersModule } from 'src/users/users.module';
     UsersModule,
   ],
   controllers: [AuthController],
-  providers: [AuthService, AuthRepository],
+  providers: [
+    AuthService,
+    AuthRepository,
+    {
+      provide: APP_GUARD,
+      useClass: AccessTokenGuard,
+    },
+  ],
   exports: [AuthService],
 })
 export class AuthModule {}
