f90076622cedae50db3d6f514b09560c04cbf6f8
rjsdn0124 <201802145@o.cnu.ac.kr>
2024-05-29 18:44:33 +0900

    [FEATURE] add validation for auth dto.

--- we-re-server/src/auth/dto/auth.dto.ts
+++ we-re-server/src/auth/dto/auth.dto.ts
@@ -1,14 +1,39 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsInt, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import {
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsStrongPassword,
+  Length,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class LocalAuthDto {
   @ApiProperty()
   @IsString()
+  @MinLength(8, {
+    message: 'account is too short',
+  })
+  @MaxLength(20, {
+    message: 'account is too long',
+  })
   @IsNotEmpty()
   account: string;
 
   @ApiProperty()
   @IsString()
+  @MaxLength(16, {
+    message: 'password is too long',
+  })
+  @IsStrongPassword({
+    minLength: 8,
+    minLowercase: 1,
+    minNumbers: 1,
+    minSymbols: 1,
+    minUppercase: 1,
+  })
   @IsNotEmpty()
   password: string;
 }
--- we-re-server/src/auth/dto/update-auth.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateLocalAuthDto } from './create-auth.dto';
-
-export class UpdateLogInDto extends PartialType(CreateLocalAuthDto) {}
