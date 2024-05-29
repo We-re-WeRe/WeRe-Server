3bfce47e0cf67cc6a3015283dd0c7797b18a2889
rjsdn0124 <201802145@o.cnu.ac.kr>
2024-05-29 19:03:10 +0900

    [FEATURE] add validation for review dto.

--- we-re-server/src/reviews/dto/create-review.dto.ts
+++ we-re-server/src/reviews/dto/create-review.dto.ts
@@ -1,30 +1,40 @@
 import { ApiProperty } from '@nestjs/swagger';
 import {
+  ArrayMaxSize,
   IsArray,
   IsInt,
   IsNotEmpty,
   IsOptional,
+  IsPositive,
   IsString,
+  Max,
+  MaxLength,
+  Min,
 } from 'class-validator';
 
 export class CreateReviewDto {
   @ApiProperty()
   @IsString()
+  @MaxLength(100)
   @IsNotEmpty()
   contents: string;
 
   @ApiProperty()
   @IsInt()
+  @Min(0)
+  @Max(10)
   @IsNotEmpty()
   starPoint: number;
 
   @ApiProperty()
   @IsInt()
+  @IsPositive()
   @IsNotEmpty()
   webtoonId: number;
 
   @ApiProperty({ type: () => [String] })
   @IsArray()
+  @ArrayMaxSize(5)
   @IsOptional()
   tags?: string[];
 
--- we-re-server/src/reviews/dto/read-review.dto.ts
+++ we-re-server/src/reviews/dto/read-review.dto.ts
@@ -1,12 +1,4 @@
 import { ApiProperty } from '@nestjs/swagger';
-import {
-  IsBoolean,
-  IsDate,
-  IsInt,
-  IsNotEmpty,
-  IsString,
-  ValidateNested,
-} from 'class-validator';
 import { ReadLikeInfoDto } from 'src/likes/dto/read-like.dto';
 import { ReadTagDto } from 'src/tags/dto/read-tag.dto';
 import { ReadUserDto } from 'src/users/dto/read-user.dto';
@@ -17,38 +9,24 @@ export class ReadReviewDto {
     raw && this.rawToDto(raw);
   }
   @ApiProperty()
-  @IsInt()
-  @IsNotEmpty()
   id: number;
 
   @ApiProperty()
-  @IsDate()
-  @IsNotEmpty()
   createdAt: Date;
 
   @ApiProperty()
-  @IsString()
-  @IsNotEmpty()
   contents: string;
 
   @ApiProperty()
-  @IsInt()
-  @IsNotEmpty()
   starPoint: number;
 
   @ApiProperty()
-  @IsBoolean()
-  @IsNotEmpty()
   isMine: boolean;
 
   @ApiProperty({ type: () => ReadLikeInfoDto })
-  @ValidateNested()
-  @IsNotEmpty()
   like: ReadLikeInfoDto;
 
   @ApiProperty({ type: () => [ReadTagDto] })
-  @ValidateNested()
-  @IsNotEmpty()
   tags: ReadTagDto[];
 
   public rawToDto(raw: any) {
@@ -70,8 +48,6 @@ export class ReadReviewAndWebtoonDto extends ReadReviewDto {
     this.rawToDto(raw);
   }
   @ApiProperty({ type: () => ReadWebtoonDto })
-  @ValidateNested()
-  @IsNotEmpty()
   webtoon: ReadWebtoonDto;
 
   public rawToDto(raw: any) {
@@ -87,8 +63,6 @@ export class ReadReviewAndUserDto extends ReadReviewDto {
     this.rawToDto(raw);
   }
   @ApiProperty({ type: () => ReadUserDto })
-  @ValidateNested()
-  @IsNotEmpty()
   user: ReadUserDto;
 
   public rawToDto(raw: any) {
