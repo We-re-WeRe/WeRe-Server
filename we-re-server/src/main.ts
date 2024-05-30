05feb0ad16f6a3e7325cb608c0701f9c7f75ce78
Geonu Lim <44187187+rjsdn0124@users.noreply.github.com>
2024-05-30 18:47:19 +0900

    [FEATURE] set CORS. (#89)

--- we-re-server/src/main.ts
+++ we-re-server/src/main.ts
@@ -6,6 +6,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: true,
+    credentials: true,
+    exposedHeaders: ['Authorization'],
+  });
   // Swagger 설정.
   const config = new DocumentBuilder()
     .setTitle('We-Re API Document')
