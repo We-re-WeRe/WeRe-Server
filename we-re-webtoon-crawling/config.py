6e23ca66fd3693304837d03afa043cc6b74b998e
rjsdn0124 <201802145@o.cnu.ac.kr>
2024-06-19 17:51:48 +0900

    [FEATURE] get db config in python.

---
+++ we-re-webtoon-crawling/config.py
@@ -0,0 +1,21 @@
+import os
+import sys
+from dotenv import load_dotenv
+
+dbConfig = {
+    "DATABASE_HOST":'',
+    "DATABASE_PORT":'',
+    "DATABASE_USER":'',
+    "DATABASE_PASSWORD":'',
+    "DATABASE_NAME":'',
+}
+env = "local"
+path = '/'.join(sys.path[0].split('/')[:-1]) + f'/we-re-server/.env.{env}'
+load_dotenv(path)
+
+
+for key in dbConfig:
+    dbConfig[key] = os.getenv(key)
+
+def getDBConfig():
+    return dbConfig
