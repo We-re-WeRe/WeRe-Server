8b30ddcf14177ae14dbcd8e7aee1640bb464dfce
rjsdn0124 <201802145@o.cnu.ac.kr>
2024-06-20 22:45:25 +0900

    [FEATURE] Implemented update webtoon repository.

--- we-re-webtoon-crawling/database/repository/webtoon.py
+++ we-re-webtoon-crawling/database/repository/webtoon.py
@@ -1,19 +1,39 @@
 from .. import connection
 
-def transformDictToKeyValueString(**kwargs):
-    def transformQueryStyleString(val):
-        return f"{val}" if type(val) == int else f'\'{val}\''
+def transformQueryStyleString(val):
+    return f"{val}" if type(val) == int else f'\'{val}\''
     
-    keys = list(map(lambda key:f'`{key}`',kwargs.keys()))
-    keyString = ', '.join(keys)
-    vals = list(map(transformQueryStyleString,kwargs.values()))
-    valueString = ', '.join(vals)
-    return keyString, valueString
-
 def createWebtoon(webtoon):
     conn = connection.connectWithDatabase()
     cur = conn.cursor()
-    key,value = transformDictToKeyValueString(**webtoon)
-    cur.execute(f"INSERT INTO webtoon({key}) VALUES({value})")
+
+    def transformDictToKeyValueString(**kwargs):
+        keys = list(map(lambda key:f'`{key}`',kwargs.keys()))
+        keyString = ', '.join(keys)
+        vals = list(map(transformQueryStyleString,kwargs.values()))
+        valueString = ', '.join(vals)
+        return keyString, valueString
+
+    keys,values = transformDictToKeyValueString(**webtoon)
+    cur.execute(f"INSERT INTO webtoon({keys}) VALUES({values})")
+    conn.commit()
+    conn.close()
+
+def updateWebtoon(webtoonUpdateContents):
+    conn = connection.connectWithDatabase()
+    cur = conn.cursor()
+
+    def transformDictToKeyEqualsValueString(id,**kwargs):
+        keyEqualsValueStringArr = []
+        for key,val in kwargs.items():
+            tempString = f'`{key}`={transformQueryStyleString(val)}'
+            keyEqualsValueStringArr.append(tempString)
+        keyEqualsValueString = ', '.join(keyEqualsValueStringArr)
+        return keyEqualsValueString
+    
+    keyEqualsValueString = transformDictToKeyEqualsValueString(**webtoonUpdateContents)
+    cur.execute(f"""
+                UPDATE WEBTOON SET {keyEqualsValueString} WHERE( id={webtoonUpdateContents["id"]})
+                """)
     conn.commit()
     conn.close()
