6aeaccb0a59e2145f197fae05078c3d3a0f57ee8
Geonu Lim <44187187+rjsdn0124@users.noreply.github.com>
2024-07-14 19:14:02 +0900

    Feature/separate database for crawling (#116)
    * [FIX] add .webp string in kakao image url.
    
    * [FEATURE] get db config in python.
    
    * [FEATURE] temporary add webtoon command in package.json.
    
    * [FEATURE] implemented git ignore for python.
    
    * [FIX] fixed exec python file name.
    
    * [FEATURE] implemented connection to mysql at python.
    
    * [FEATURE] Edited code for connection.
    
    * [FEATURE] Implemented insert webtoon repository. and removed __init__ file.
    
    * [FEATURE] Implemented update webtoon repository.
    
    * [FEATURE] Implemented get webtoon logic.
    
    * [FEATURE] separating crawling feature from original.
    
    * [FIX] changed directory and file name.
    
    * [FEATURE] separated naver crawling feature from original.
    
    * [FIX] fixed env annotated.
    
    * [FIX] fixed author and painter logic.
    
    * [FIX] changed wrong field name to be snake case.
    
    * [DELETE] removed previous webtoon crawling codes.
    
    * [FIX] fixed wrong query command.
    
    * [FEATURE] added escape double quote for explain.
    
    * [FEATURE] implemented main.py for crawling.

--- we-re-webtoon-crawling/webtoon/kakao.py
+++ we-re-webtoon-crawling/webtoon/kakao.py
@@ -1,21 +1,26 @@
 import requests
 import json
-days = ["MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY","SATURDAY","SUNDAY"]
-providingCompany = "kakao"
-apiUrl = 'http://localhost:3000'
 
+providingCompany = "k"
 genreDictionary = { "#드라마":"DRAMA", "#코믹/일상":"COMIC/DAILY","#액션/무협":'ACTION', "#공포/스릴러":'THRILL', "#로맨스":"PURE", "#판타지 드라마":'FANTASY/DRAMA', "#로맨스 판타지":'ROMANSE/FANTASY', "#학원/판타지":"ACADEMY/FANTASY"}
-for day in days:
-    response = requests.get(f"https://gateway-kw.kakao.com/section/v1/timetables/days?placement=timetable_{day.lower()[:3]}")
+webtoonListURL = "https://gateway-kw.kakao.com/section/v1/timetables/days"
+webtoonPageURL = "https://webtoon.kakao.com/content"
+webtoonDetailURL = "https://gateway-kw.kakao.com/decorator/v2/decorator/contents"
+
+def getParsedKakaoWebtoonList(day):
+    webtoonArray = []
+    query = f"placement=timetable_{day.lower()[:3]}"
+    response = requests.get(f"{webtoonListURL}?{query}")
     parsedData = json.loads(response.text)
+    
     webtoonsAtThatDay = parsedData["data"][0]["cardGroups"][0]["cards"]
     for webtoon in webtoonsAtThatDay:
         content = webtoon["content"]
         id = content["id"]
         title = content["title"]
         seoId = content["seoId"]
-        webtoonUrl = f"https://webtoon.kakao.com/content/{seoId}/{id}"
-        thumbnailUrl = content["featuredCharacterImageB"]
+        webtoonUrl = f"{webtoonPageURL}/{seoId}/{id}"
+        thumbnailUrl = content["featuredCharacterImageB"]+'.webp'
         authorAndPainter = content["authors"]
         authorArr = []
         painterArr = []
@@ -34,17 +39,13 @@ for day in days:
         author = ', '.join(authorArr if len(authorArr) > 0 else originalStoryArr)
         painter = ', '.join(painterArr if len(painterArr) > 0 else publisherArr)
 
-        if requests.patch(f'{apiUrl}/webtoons?id={id}',data={'id':id, 'imageURL': thumbnailUrl,'webtoonURL': webtoonUrl,'author': author,'painter': painter}).status_code == 200:
-            print(f"Patch is completed {id}")
-        else:
-            webtoonInfoUrl = f"https://gateway-kw.kakao.com/decorator/v2/decorator/contents/{id}/profile"
-            response = requests.get(webtoonInfoUrl)
-            parsedInfoData = json.loads(response.text)["data"]
-            
-            explain = parsedInfoData["synopsis"]
-            genre = genreDictionary[parsedInfoData["seoKeywords"][2]]
-            response = requests.post(f'{apiUrl}/webtoons',data={'id':id,'title': title,'imageURL': thumbnailUrl,'webtoonURL': webtoonUrl,'author': author,'painter': painter,'providingCompany': providingCompany, 'day': day,'genre': genre,'explain': explain})
-            if response.status_code == 201:
-                print(f"Create Webtoon is completed {id}")
-            else:
-                print(response.text)
+        webtoonInfoUrl = f"{webtoonDetailURL}/{id}/profile"
+        response = requests.get(webtoonInfoUrl)
+        parsedInfoData = json.loads(response.text)["data"]
+        
+        explain = parsedInfoData["synopsis"].replace("\"","\\\"")
+        genre = genreDictionary[parsedInfoData["seoKeywords"][2]]
+        result = {'id':id,'title': title,'image_url': thumbnailUrl,'webtoon_url': webtoonUrl,'author': author,'painter': painter,'providing_company': providingCompany, 'day': day.lower()[:3],'genre': genre,'explain': explain}
+        
+        webtoonArray.append(result)
+    return webtoonArray
