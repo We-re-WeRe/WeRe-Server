af929549efd4b1f10246ac946779e3805f5678fc
Geonu Lim <44187187+rjsdn0124@users.noreply.github.com>
2024-07-27 18:01:12 +0900

    Feature/recently watched targets (#129)
    * [CHANGE] changed visited list logic to use for recently viewed.
    
    * [FIX] separate again vlist and recent view list.
    
    * [FEATURE] refactored function to use with recent list and implemented set recent cookie logic.
    
    * [FEATURE] implement get and update recent visited list logic.
    
    * [FEATURE] implemented get recent visited webtoon list api.

--- we-re-server/src/webtoons/webtoons.controller.ts
+++ we-re-server/src/webtoons/webtoons.controller.ts
@@ -37,7 +37,10 @@ import {
 } from 'src/entities/webtoon.entity';
 import { Cookies, Public, UserId } from 'src/utils/custom_decorators';
 import { Response } from 'express';
-import { VISITED_LIST_COOKIE_KEY } from 'src/utils/types_and_enums';
+import {
+  RECENT_VIEWED_COOKIE_KEY,
+  VISITED_LIST_COOKIE_KEY,
+} from 'src/utils/types_and_enums';
 
 @ApiTags('Webtoons')
 @Controller('webtoons')
@@ -59,18 +62,43 @@ export class WebtoonsController {
     @UserId() userId: number,
     @Query('id') id: number,
     @Cookies(VISITED_LIST_COOKIE_KEY) visitedList: string,
+    @Cookies(RECENT_VIEWED_COOKIE_KEY) recentList: string,
     @Res({ passthrough: true }) res: Response,
   ): Promise<ReadWebtoonDetailDto> {
     if (!id) throw new CustomBadTypeRequestException('id', id);
 
-    if (!this.checkVistedListFromCookie(visitedList, id)) {
+    const result = await this.webtoonsService.findOneDetailById(id, userId);
+
+    // 최근 및 조회 목록 확인하고 업데이트.
+    if (!!result && !this.isIdInUnderBarString(visitedList, id)) {
       const updatedVisitedList = this.updateVisitedList(visitedList, id);
+      // 새로 조회할 때마다 조회수 업데이트 맞아...? 고민 ㄱ
       this.webtoonsService.updateViewCount(id);
-      this.setVisitedListInCookie(res, updatedVisitedList);
+      this.setDataInCookie(res, VISITED_LIST_COOKIE_KEY, updatedVisitedList, 1);
     }
+    const updatedRecentList = this.updateRecentList(recentList, id);
+    this.setDataInCookie(res, RECENT_VIEWED_COOKIE_KEY, updatedRecentList, 14);
 
     const result = await this.webtoonsService.findOneDetailById(id, userId);
     return result;
+  }
+
+  @ApiOperation({
+    summary: '최근 본 웹툰 목록(10개) 전달 api',
+  })
+  @ApiOkResponse({
+    description: 'Request Success',
+    type: [ReadWebtoonThumbnailDto],
+  })
+  @Public()
+  @Get('list/recent')
+  async findManyRecentThumbnail(
+    @Cookies(RECENT_VIEWED_COOKIE_KEY) recentList: string,
+  ): Promise<ReadWebtoonThumbnailDto[]> {
+    const ids = this.decodeUnderbarStringToList(recentList).map(
+      this.decodeBase32ToNumber,
+    );
+    return this.webtoonsService.findManyThumbnailByIds(ids);
   }
 
   @ApiOperation({
@@ -209,33 +237,97 @@ export class WebtoonsController {
     return await this.webtoonsService.deleteWebtoon(id);
   }
 
-  checkVistedListFromCookie = (visitedList: string, id: number): boolean => {
-    if (!!visitedList) {
-      const vlistJson = JSON.parse(visitedList);
-      return !!vlistJson[id];
-    } else {
-      return false;
-    }
+  /**
+   * id를 32진수로 인코딩
+   * @param id webtoonId
+   * @returns encodedId
+   */
+  encodeNumberToBase32 = (id: number): string => id.toString(32);
+
+  /**
+   * 32진수 문자열을 숫자로 디코딩
+   * @param base32String 32진수로 표현된 문자열
+   * @returns webtoonId
+   */
+  decodeBase32ToNumber = (base32String: string): number =>
+    parseInt(base32String, 32);
+
+  /**
+   * 쿠키에 저장된 '_' 기준 구분 배열 문자열을 list로 반환하는 함수.
+   * @param underbarString cookie에 저장된 '_'로 구분된 문자열
+   * @returns webtoonId list
+   */
+  decodeUnderbarStringToList = (underbarString: string): string[] => {
+    return underbarString?.split('_') || [];
   };
 
+  /**
+   * 문자열에 특정 id가 있는지 확인하는 메소드.
+   * @param underbarString cookie에 저장된 '_'로 구분된 문자열
+   * @param id webtoon id
+   * @returns boolean
+   */
+  isIdInUnderBarString = (underBarString: string, id: number): boolean => {
+    const vlist = this.decodeUnderbarStringToList(underBarString);
+    return vlist.indexOf(id.toString(32)) >= 0;
+  };
+
+  /**
+   * 조회 목록을 업데이트하는 함수.
+   * @param visitedList 당일 조회한 웹툰 id 목록이 담긴 문자열.
+   * @param id webtoon id
+   * @returns 업데이트된 문자열.
+   */
   updateVisitedList = (visitedList: string, id: number): string => {
-    let vlistJson: any;
-    if (!!!visitedList) {
-      vlistJson = {};
-    } else {
-      vlistJson = JSON.parse(visitedList);
-    }
-    vlistJson[id] = 0b1;
+    const idEncoded32 = this.encodeNumberToBase32(id);
 
-    return JSON.stringify(vlistJson);
+    return (!!visitedList ? visitedList + '_' : '') + idEncoded32;
   };
 
-  setVisitedListInCookie = (res: Response, value: string): void => {
+  /**
+   * 최근 본 목록 업데이트
+   * @param recentList 최근 본 웹툰 목록.
+   * @param id webtoon id
+   * @returns 업데이트된 문자열
+   */
+  updateRecentList = (recentList: string, id: number): string => {
+    const idEncoded32 = this.encodeNumberToBase32(id);
+    const rlist = this.decodeUnderbarStringToList(recentList);
+    const idx = rlist.indexOf(idEncoded32);
+
+    if (idx >= 0)
+      [rlist[idx], rlist[rlist.length - 1]] = [
+        rlist[rlist.length - 1],
+        rlist[idx],
+      ];
+    else {
+      while (rlist.length >= 10) {
+        rlist.shift();
+      }
+      rlist.push(idEncoded32);
+    }
+
+    return rlist.join('_');
+  };
+
+  /**
+   * 쿠키에 업데이트된 데이터 저장.
+   * @param res response
+   * @param key cookie key
+   * @param value cookie value
+   * @param expires expires date
+   */
+  setDataInCookie = (
+    res: Response,
+    key: string,
+    value: string,
+    expires: number,
+  ): void => {
     const today = new Date(new Date().toLocaleDateString());
-    today.setDate(today.getDate() + 1);
+    today.setDate(today.getDate() + expires);
     today.setHours(today.getHours() + 9);
 
-    res.cookie(VISITED_LIST_COOKIE_KEY, value, {
+    res.cookie(key, value, {
       path: '', // cookie path는 domain을 따라야하는 듯하다..? 빈 값을 넣어서 webtoons path의 쿠키를 가져온다.
       httpOnly: true,
       expires: today,
